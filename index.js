b367c9fe3cbf8dc7158f1df6a86817357cca3734
alextwomey <alexptwomey@gmail.com>
2020-11-08 18:24:03 -0500

    Final Version
    Dynamic flag generator using html canvas.

---
+++ index.js
@@ -0,0 +1,54 @@
+//alex twomey
+var selectInfo = {
+	"choices":[
+		{
+        "key": "Main",
+        "option_1": "Diagonal Split",
+        "option_2": "Middle Split",
+        "description": "Select a flag style",
+        "depth": 0
+      },
+		{
+        "key": "Diagonal Split",
+        "option_1": "Red",
+        "option_2": "Blue",
+        "description": "Select a background color",
+        "depth": 1
+      },
+		{
+        "key": "Middle Split",
+        "option_1": "Green",
+        "option_2": "Orange",
+        "description": "Select a background color",
+        "depth": 1
+      },
+		{
+        "key": "Red",
+        "option_1": "Circle",
+        "option_2": "Square",
+        "description": "Select a foreground object shape",
+        "depth": 2
+      },
+		{
+        "key": "Blue",
+        "option_1": "Triangle",
+        "option_2": "Diamond",
+        "description": "Select a foreground object shape",
+        "depth": 2
+      },
+		{
+        "key": "Green",
+        "option_1": "Oval(horizontal)",
+        "option_2": "Triangle(upside down)",
+        "description": "Select a foreground object shape",
+        "depth": 2
+      },
+		{
+        "key": "Orange",
+        "option_1": "Oval(verticle)",
+        "option_2": "Rectangle(horizontal)",
+        "description": "Select a foreground object shape",
+        "depth": 2
+      },
+	]
+}
